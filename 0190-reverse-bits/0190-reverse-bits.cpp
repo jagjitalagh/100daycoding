045baa0f2d949e3b10ea9bf99cd5f38bd8de8290
Jagjit Alagh <131643417+jagjitalagh@users.noreply.github.com>
2024-01-25 08:45:01 +0530

    Time: 5 ms (13.85%), Space: 6.9 MB (7.52%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+         uint32_t v = 0;
+        for(int i=0;i<=31;i++){
+            if(n & (1<<i)){
+                v = v | (1<<(31-i));
+            }
+        }
+        
+        return v;
+    }
+};
